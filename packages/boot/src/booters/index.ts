profnandaa/loopback-next

feat(boot): add a booter for life cycle scripts
See https://github.com/strongloop/loopback-next/issues/2034

packages/boot/src/booters/index.ts
@@ -10,3 +10,4 @@ export * from './datasource.booter';
 export * from './repository.booter';
 export * from './service.booter';
 export * from './application-metadata.booter';
+export * from './lifecyle-script.booter';

yuenshui/loopback4

packages/boot/src/booters/index.ts
@@ -10,3 +10,4 @@ export * from './datasource.booter';
 export * from './repository.booter';
 export * from './service.booter';
 export * from './application-metadata.booter';
+export * from './lifecyle-observer.booter';